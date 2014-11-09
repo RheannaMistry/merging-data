6b10c2cd454a35e50b03a87bc0b436340cdf42b0
Rheanna Mistry <Rhekmist@uw.edu>
2014-11-08 23:18:51 -0800

    Update

--- js/app.js
+++ js/app.js
@@ -5,3 +5,15 @@
     Add code here to create a new Angular application and controller.
     The array of employee objects is already in a global variable named 'pawneeEmployees'
 */
+
+var AddressBook = angular.module('AddressBook', []);
+    AddressBook.controller('AddressController', function($scope) {
+        $scope.employees = pawneeEmployees;
+        $scope.sortCol = 'lastName';
+        $scope.sortBy = function(sortCol) {
+            if ($scope.sortCol != sortCol) {
+                $scope.sortCol = sortCol;
+            }
+        };
+});
+
